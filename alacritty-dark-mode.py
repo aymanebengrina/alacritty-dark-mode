d23f5a748b6ab0a75f072e83888205c8ab807be1
Aymane Bengrina <bengrina.aymane@gmail.com>
2019-12-01 22:49:54 +0100

    Update and rename alacritty_dark.py to alacritty-dark-mode.py
    Ajout de commentaires.

--- alacritty-dark-mode.py
+++ alacritty-dark-mode.py
@@ -1,21 +1,25 @@
 #! /usr/bin/env python3
+# alacritty-dark-mode.py: A script to switch between dark and light color schemes in alacritty
 
 import re
 import os
 
+# Opens alacritty's config file in read only mode, and save its contents into a variable
 doc = open(os.path.expanduser('~/.config/alacritty/alacritty.yml'), 'r' )
 file = doc.read()
+# Closing the document and opening it again in write mode
 doc.close()
 doc = open(os.path.expanduser('~/.config/alacritty/alacritty.yml'), 'w' )
 
-
+# The two regular expressions to detect if Alacritty is in dark or light mode
 light = re.search(r'\*light', file)
 dark = re.search(r'\*dark', file)
 
+# If alacritty is in light mode, switch to dark mode
 if light:
 	new_file = re.sub(r"\*light", "*dark", file, flags = re.M)
 	doc.write(new_file)
-	
+# If alacritty is in light mode, switch to dark mode	
 if dark:
 	new_file = re.sub(r"\*dark", "*light", file, flags = re.M)
 	doc.write(new_file)
